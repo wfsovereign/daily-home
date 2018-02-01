ecd757c2be1f6277273ff08362401df193d623fb
wfsovereign <wfsovereign@outlook.com>
2018-02-01 15:06:22 +0800

    add antd

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'antd-mobile';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>hello world</Text>
+        <Button type="primary">antd-mobile button</Button>
       </View>
     );
   }
